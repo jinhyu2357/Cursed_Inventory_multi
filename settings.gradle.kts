809089f8bbea04fbdb3624e4049be0fa6085e1f0
jinhyu2357 <jinhyu2357@gmail.com>
2026-02-26 22:05:38 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Cursed_Inventory"
---
+++ src/main/kotlin/org/example/jinhhyu/cursed_Inventory/Cursed_Inventory.kt
@@ -0,0 +1,14 @@
+package org.example.jinhhyu.cursed_Inventory
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Cursed_Inventory : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
